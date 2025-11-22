97ae79d02aa3a70999108ef10d2d5d8034168532
Reuben Bowlby <reuben@hummbl.io>
2025-11-22 16:54:21 -0500

    feat(infra): add terraform configuration for GKE
    - Added main.tf with VPC and GKE cluster definitions
    - Added variables.tf and outputs.tf
    - Configured Workload Identity and VPC-native networking

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,28 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "cluster_name" {
+  description = "Name of the GKE cluster"
+  type        = string
+  default     = "engine-ops-cluster"
+}
+
+variable "node_count" {
+  description = "Number of nodes per zone"
+  type        = number
+  default     = 1
+}
+
+variable "machine_type" {
+  description = "Machine type for nodes"
+  type        = string
+  default     = "e2-medium"
+}
